77de5473bf8010fa41d5ffeb445d2caebf615469
markof94 <markofajler@gmail.com>
2021-08-26 20:06:04 +0200

    refactor

--- src/Utilities/getTexturesFromAnimation.ts
+++ src/Utilities/getTexturesFromAnimation.ts
@@ -3,18 +3,16 @@ import { Texture } from "pixi.js";
 const getTexturesFromAnimation = (animationName: string): Texture[] => {
   const textures: Texture[] = [];
   let i = 0;
-  let valid = true;
 
-  while (valid) {
+  while (1) {
     const texture = Texture.from(`${animationName}-${i}.png`);
 
     if (!texture || !texture.valid) {
-      valid = false;
       break;
-    } else {
-      textures.push(texture);
-      i += 1;
     }
+
+    textures.push(texture);
+    i += 1;
   }
 
   return textures;
